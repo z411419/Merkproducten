370facbd1ca90c6794b409a9da48462825947b9e
Tom <tom.claes82@gmail.com>
2017-09-13 12:26:03 +0200

    Product class

---
+++ src/be/oak3/model/Parfum.java
@@ -0,0 +1,10 @@
+package be.oak3.model;
+
+public abstract class Parfum extends Product {
+
+
+    public Parfum(int productNummer, String merk, String naam, int volume, double prijs) {
+        super(productNummer, merk, naam, volume, prijs);
+
+    }
+}
